352911b0ce56432df9308e9e50d086d654ca9aa8
Shawn Bernard <ssbernard@outlook.com>
2025-01-09 15:21:14 -0400

    Added Crouch button, Jump button

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,16 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
+using static GameInput;
 
-public class InputManager : MonoBehaviour
+public class InputManager : MonoBehaviour, IGameplayActions
+//if not using "using static GameInput;" than it's GameInput.IGameplayActions
 {
+    GameInput gameInput;
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        Debug.Log("Crouch was pressed");
+        CrouchEvent?.Invoke();
+    }
+    //Have to have OnCrouch and OnJump when using IGameplayActions
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Jump was pressed");
+            JumpEvent?.Invoke();
+        }
+    }
+
+    #region Public Actions
+
+    private Action JumpEvent;
+    private Action CrouchEvent;
+
+    #endregion
+
+    void Start()
+    {
+        gameInput = new GameInput();
+
+        gameInput.Gameplay.Enable();
+
+        gameInput.Gameplay.SetCallbacks(this);
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-            Debug.Log("Space key was pressed");
-        }
-        
+
     }
 }
